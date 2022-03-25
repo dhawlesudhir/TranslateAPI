352ad6422993ea1f3da61b2c6334da12b4f29db8
dhawlesudhir <34824916+dhawlesudhir@users.noreply.github.com>
2022-03-25 09:01:16 +0530

    added translate feature

---
+++ translation-assignment/app/Http/Controllers/TranslateController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\GoogleTranslator;
+use Illuminate\Http\Request;
+
+class TranslateController extends Controller
+{
+
+
+    /**
+     * Accepting Text and TargetLaguage code
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function index(GoogleTranslator $gt,Request $request)
+    {  
+        return $gt->translate($request->input('text'),$request->input('translateTo'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create() 
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+}
--- translation-assignment/routes/api.php
+++ translation-assignment/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TranslateController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+// API endpoint 
+Route::resource('/translate',TranslateController::class,['only' => 'index']);
